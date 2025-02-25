aa736a7833236e83fbe405c983cba139ae4e7c03
EC2 Default User <ec2-user@ip-172-31-22-15.ec2.internal>
2025-02-25 06:46:02 +0000

    a new files are add

---
+++ sample4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to check if it's prime: ");
+        int num = sc.nextInt();
+        boolean isPrime = true;
+        
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        
+        if (num <= 1) {
+            System.out.println(num + " is not a prime number.");
+        } else if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
+
---
+++ sample5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of terms in Fibonacci sequence: ");
+        int n = sc.nextInt();
+        
+        int a = 0, b = 1;
+        System.out.print("Fibonacci sequence: ");
+        
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            int nextTerm = a + b;
+            a = b;
+            b = nextTerm;
+        }
+    }
+}
+
